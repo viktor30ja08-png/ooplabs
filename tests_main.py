f3ea30132ae120a3ddf185e9477198b8a2195ea3
viktor30ja08-png <viktor30ja08@gmail.com>
2026-04-28 17:53:14 +0300

    Update tests_main.py
    Updated comments in test cases for clarity.

--- tests_main.py
+++ tests_main.py
@@ -1,11 +1,10 @@
 import unittest
-
-
+from main import validate_matrix, multiply_matrix_by_scalar, get_sum_of_min_in_columns
 
 class TestMatrixOperations(unittest.TestCase):
 
     def setUp(self):
-        
+        # Дані для тестування
         self.matrix_b = [
             [1, -5,   8,  12],
             [4,  2, -10,   3],
@@ -20,24 +19,22 @@ class TestMatrixOperations(unittest.TestCase):
             [12,  6, -30,   9],
             [-21, 27,   0, -45]
         ]
-        
         result = multiply_matrix_by_scalar(self.matrix_b, self.scalar_a)
         self.assertEqual(result, expected_result)
 
     def test_get_sum_of_min_in_columns(self):
         """Перевіряємо, чи правильно рахується сума найменших елементів стовпців"""
         matrix_c = multiply_matrix_by_scalar(self.matrix_b, self.scalar_a)
-        
         result = get_sum_of_min_in_columns(matrix_c)
         self.assertEqual(result, -111)
 
     def test_empty_matrix_validation(self):
-        """Перевіряємо, чи програма видає помилку при порожній матриці"""
+        """Перевіряємо, чи спрацьовує помилка при порожній матриці"""
         with self.assertRaises(ValueError):
             validate_matrix([])
 
     def test_invalid_shape_matrix(self):
-        """Перевіряємо, чи програма видає помилку при матриці-франкенштейні"""
+        """Перевіряємо, чи спрацьовує помилка при нерівних рядках матриці"""
         invalid_matrix = [
             [1, 2, 3],
             [4, 5]     
